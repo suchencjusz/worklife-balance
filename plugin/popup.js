582fe458da8c2a011c25563e231ec35338c6e90e
kacpereqo <remzak.k@gmail.com>
2023-12-15 22:34:51 +0100

    feat: login button

--- plugin/popup.js
+++ plugin/popup.js
@@ -1,12 +1,32 @@
 // adding a new bookmark row to the popup
-const addNewBookmark = () => {};
+const main = document.querySelector("#main");
 
-const viewBookmarks = () => {};
+const isSignedIn = () => {
+  return false;
+};
 
-const onPlay = e => {};
+function openLoginTab() {
+  window.open("https://www.google.com", "_blank");
+}
 
-const onDelete = e => {};
+function loginView() {
+  main.innerHTML = `<div class="login">
+    <button id="login__button"">Login</button>
+    <p id="login__text">login to start tracking your life!</p>
+  </div>
+  `;
 
-const setBookmarkAttributes =  () => {};
+  const loginButton = document.querySelector("#login__button");
+  loginButton.addEventListener("click", openLoginTab);
+}
 
 document.addEventListener("DOMContentLoaded", () => {});
+
+function init() {
+  if (isSignedIn()) {
+  } else {
+    loginView();
+  }
+}
+
+init();
