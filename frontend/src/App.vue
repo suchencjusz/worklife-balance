f06fb1c580cb00d9afca48e02869e266fbb3f8b8
kacpereqo <remzak.k@gmail.com>
2023-12-15 22:58:48 +0100

    feat: navbar

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import TheNavBar from './components/TheNavBar.vue'
+</script>
+
+<template>
+  <TheNavBar />
+  <RouterView />
+</template>
+
+<style scoped></style>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Roboto', sans-serif;
+  background-color: #d4ebf2;
+}
+
+#app {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+</style>
