2da77f0d8382b752f11c7f68ddb01edddb1a50a5
suchencjusz <sucheta348@gmail.com>
2023-12-16 03:12:47 +0100

    ll

--- request_test.py
+++ request_test.py
@@ -1,29 +1,44 @@
 import requests
 import ujson
 from pydantic import BaseModel
+import random
 
-class ActivityIn(BaseModel):
-    id_user: str
-    url: str
-    unix_timestamp_start: int
-    unix_timestamp_end: int
+def x():
+    class ActivityIn(BaseModel):
+        id_user: str
+        url: str
+        unix_timestamp_start: int
+        unix_timestamp_end: int
+        duration: int
 
-# Create an instance of ActivityIn
-activity = ActivityIn(
-    id_user="user123",
-    url="http://example.com",
-    unix_timestamp_start=1633024800,  # Example timestamp
-    unix_timestamp_end=1633028400     # Example timestamp
-)
+    random_timestamp = random.randint(1633024800, 1635025800)
+    duration = random.randint(0, 250)
 
-activity_json = activity.model_dump_json()
+    # Create an instance of ActivityIn
+    activity = ActivityIn(
+        id_user="essa",
+        url="http://example.com",
+        unix_timestamp_start=random_timestamp,
+        unix_timestamp_end=random_timestamp + duration,
+        duration=duration
+    )
 
-# URL of the API endpoint
-url = "http://localhost:8080/api/activities/add"
+    activity_json = activity.model_dump_json()
+
+    # URL of the API endpoint
+    url = "http://localhost:8080/api/activities/add"
+
+    requests.post(url, data=activity_json, headers={'Content-Type': 'application/json'})
+
+    print(activity_json)
+
+for i in range(100):
+    x()
+
 
 # Send a POST request with the JSON data
-response = requests.post(url, data=activity_json, headers={'Content-Type': 'application/json'})
+# response = requests.post(url, data=activity_json, headers={'Content-Type': 'application/json'})
 
-# Print the responseE
-print(response.status_code)
-print(response.text)
+# # Print the responseE
+# print(response.status_code)
+# print(response.text)
